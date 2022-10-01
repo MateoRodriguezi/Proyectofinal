6aa3949dc2041e10f0fe2a7ecfeab82450acb8e4
Mateo Rodriguez <mateorodriguezpintos1902@gmail.com>
2022-10-01 02:42:38 -0300

    correcion de errores

--- Blog_Coder/Blogapp/urls.py
+++ Blog_Coder/Blogapp/urls.py
@@ -8,10 +8,7 @@ urlpatterns = [
     path('pages/', views.ArticuloListView.as_view(), name='Articulos'),
     path('about/', views.about, name='Acerca de'),
     path(r'^(?P<pk>/d+)$', views.ArticuloDetailView.as_view(), name="Detail"),
-    path(r'^nuevo$', views.ArticuloCreateView.as_view(
-        model=Articulo, success_url="/blog-viajes/pages"), name="New"),
-    path(r'^editar/(?P<pk>/d+)$', views.ArticuloUpdateView.as_view(model=Articulo,
-         success_url="/blog-viajes/pages"), name="Edit"),
-    path(r'^borrar/(?P<pk>/d+)$', views.ArticuloDeleteView.as_view(model=Articulo,
-         success_url="/blog-viajes/pages"), name="Delete"),
+    path(r'^nuevo$', views.ArticuloCreateView.as_view(model=Articulo, success_url="/blog-viajes/pages/"), name="New"),
+    path('editar/<int:pk>/', views.ArticuloUpdateView.as_view(model=Articulo,success_url="/blog-viajes/pages/"), name="Edit"),
+    path('eliminar/<int:pk>/', views.ArticuloDeleteView.as_view(model=Articulo,success_url="/blog-viajes/pages/"), name="Delete"),
 ]
--- Blog_Coder/Blogapp/views.py
+++ Blog_Coder/Blogapp/views.py
@@ -2,56 +2,43 @@ from django.shortcuts import render, redirect, reverse
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from http.client import HTTPResponse
 from typing import Dict
 from urllib.request import HTTPRedirectHandler
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
 from Blogapp.models import *
 
 # Create your views here.
 
-
-@login_required
 def home(request):
-    return render(request, "Blogapp/home.html")
+    return render (request, "Blogapp/home.html")
 
-
-@login_required
 def about(request):
-    return render(request, "Blogapp/about.html")
-
+    return render (request, "Blogapp/about.html")
 
 def pages(request):
-    return render(request, "Blogapp/pages.html")
-
+    return render (request, "Blogapp/pages.html")
 
 class ArticuloListView(ListView):
     model = Articulo
     template_name = 'Blogapp/pages.html'
 
-
 class ArticuloDetailView(DetailView):
     model = Articulo
     template_name = 'Blogapp/page_id.html'
 
-
-class ArticuloCreateView(CreateView):
+class ArticuloCreateView(CreateView, LoginRequiredMixin):
     model = Articulo
-    succes_url = reverse_lazy('Articulos')
-    fields = ['titulo', 'sub_titulo', 'fecha',
-              'autor', 'email_autor', 'cuerpo', 'imagen']
+    fields = ['titulo', 'sub_titulo', 'fecha', 'autor', 'email_autor', 'cuerpo', 'imagen']
+    succes_url = reverse_lazy('Inicio')
 
-
-class ArticuloUpdateView(UpdateView):
+class ArticuloUpdateView(UpdateView, LoginRequiredMixin):
     model = Articulo
-    succes_url = reverse_lazy('Articulos')
-    fields = ['titulo', 'sub_titulo', 'fecha',
-              'autor', 'email_autor', 'cuerpo', 'imagen']
+    succes_url = reverse_lazy('Inicio')
+    fields = ['titulo', 'sub_titulo', 'fecha', 'autor', 'email_autor', 'cuerpo', 'imagen']
 
-
-class ArticuloDeleteView(DeleteView):
+class ArticuloDeleteView(DeleteView, LoginRequiredMixin):
     model = Articulo
-    succes_url = reverse_lazy('Articulos')
+    succes_url = reverse_lazy('Inicio')
