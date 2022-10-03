fdf44ad1e0583b443b2b0623a746bbce79818e52
Mateo Rodriguez <mateorodriguezpintos1902@gmail.com>
2022-10-02 22:39:32 -0300

    se agrega h3 en caso que no haya imagenes en el articulo

--- Blog_Coder/Blogapp/tests.py
+++ Blog_Coder/Blogapp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import random
+import string
 
-# Create your tests here.
+from django.test import TestCase
+from Blogapp.models import Articulo
+
+class ArticuloTestCase(TestCase):
+    pass
+
+    def test_crear_articulo():
+        #Test 1: Comprobar crear articulo con letras random
+        pass
+
+
+
